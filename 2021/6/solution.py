58f9d2b3a6260b51a36fc7ec88db42ebc3598bc4
JonM0 <61517667+JonM0@users.noreply.github.com>
2021-12-06 13:09:45 +0100

    Add day 6

---
+++ 2021/6/solution.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+first_star_gens = 80
+second_star_gens = 256
+data = Counter(int(n) for n in open('input.txt').readline().split(','))
+
+def next_gen(g):
+    return (g[1], g[2], g[3], g[4], g[5], g[6], g[0]+g[7], g[8], g[0])
+
+# first star
+gen = data
+for _ in range(first_star_gens):
+    gen = next_gen(gen)
+
+print(f'after {first_star_gens} generations: {sum(gen)}')
+
+# second star
+for _ in range(first_star_gens, second_star_gens):
+    gen = next_gen(gen)
+
+print(f'after {second_star_gens} generations: {sum(gen)}')
